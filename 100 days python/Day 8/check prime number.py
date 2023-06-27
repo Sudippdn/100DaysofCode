20df2dc312739439cb8e411a6c94d9c7a581d734
Sudip Pradhan <104957400+Sudippdn@users.noreply.github.com>
2023-06-27 07:05:14 +0545

    Create check prime number.py

---
+++ 100 days python/Day 8/check prime number.py
@@ -0,0 +1,12 @@
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, integer): 
+        if integer%i == 0:
+            is_prime = False
+    if is_prime:
+        print("This is prime number")
+    else:
+        print("This is not prime number")
+result = 0
+integer = int(input("Enter an integer: "))
+prime_checker(number = integer)
