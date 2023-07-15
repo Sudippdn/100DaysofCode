64d694b10b241e192cf6b6702b74617b9ead3a0d
Sudip Pradhan <104957400+Sudippdn@users.noreply.github.com>
2023-07-15 23:49:18 +0545

    Add files via upload

---
+++ 100 days python/Day 11/Blackjack game.py
@@ -0,0 +1,43 @@
+card_value = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+import random
+results = ""
+choice = ""
+
+for items in card_value:
+    rand1 = int(random.choice(card_value))
+    rand2 = int(random.choice(card_value))
+    rand3 = int(random.choice(card_value))
+    break
+addRand = rand1 + rand2
+print(f"Your cards: [{rand1}, {rand2}], current score = {addRand}")
+print(f"Computer's first card: {rand3}")
+get_anotherCard = True
+while get_anotherCard:
+    choice = input("Do you want another card? (y/n): ")
+    while addRand <=21:
+        if choice == 'y':
+            newRand = random.choice(card_value)
+            newAdd = addRand + newRand
+            print(f"Your final card: [{rand1}, {rand2}, {newRand}], final score = {newAdd}")
+            break
+        elif choice == 'n':
+            get_anotherCard = False
+    if newAdd >21:
+        print("Sadly you exceeded 21, You lost!!!")
+        get_anotherCard = False
+    elif (newAdd == 21):
+        print("Congratulations, You won!!!")
+    else:
+        rand4 = random.choice(card_value)
+        rand5 = random.choice(card_value)
+        CompAdd = int(rand4) + int(rand5) + rand3
+        print(f"Computer final cards: [{rand4}, {rand5}], Computer score = {CompAdd}")
+    
+        if (newAdd == CompAdd):
+            print("Game is Draw!!!")
+        elif (newAdd < CompAdd):
+            print("Computer wins")
+        elif (newAdd > CompAdd):
+            print("Congratulation, you won")
+    break
+        
