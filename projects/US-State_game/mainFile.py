ea7eb5bbe2b18220ed0c620e3af032a85ae081ed
Sudip Pradhan <104957400+Sudippdn@users.noreply.github.com>
2024-02-24 07:53:17 +0545

    Add files via upload

---
+++ projects/US-State_game/mainFile.py
@@ -0,0 +1,12 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+image = "blank_states_img.gif"
+# screen.addshape(image)
+
+screen.addshape(image)
+turtle.shape(image)
+
+
+screen.exitonclick()
